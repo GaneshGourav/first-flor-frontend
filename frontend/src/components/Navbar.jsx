10b336125a5020c55f42eb594bd48debf59e491e
Ganesh Kumar <mrganeshofficial8@gmail.com>
2024-06-25 18:35:53 +0530

    navbar responsiveness added

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,23 +5,25 @@ export const Navbar = () => {
     <>
       <nav>
         <div className="w-full  flex justify-between items-center px-2 py-3">
-          <div className="px-20 w-[50%]">
+          <div className="md:px-20 w-[40%] md:w-[50%]">
             <img src={logo} alt="" />
           </div>
-          <div className=" w-[40%] flex justify-around items-center text-blue-500 ">
-            <a href="#" className="font-[600] text-[18px]">
-              Buy
-            </a>
-            <a href="#" className="font-[600] text-[18px]">
-              Rent
-            </a>
-            <a href="#" className="font-[600] text-[18px]">
-              Sell
-            </a>
-            <a href="#" className="font-[600] text-[18px]">
-              PG
-            </a>
-            <button className="  px-8 py-2 bg-blue-600 text-white rounded-[5px] text-[14px] font-[600]">
+          <div className=" w-[100%] md:w-[40%] flex justify-around items-center text-blue-500   ">
+            <div className="md:flex justify-around w-[50%] hidden">
+              <a href="#" className="font-[600] text-[18px]">
+                Buy
+              </a>
+              <a href="#" className="font-[600] text-[18px]">
+                Rent
+              </a>
+              <a href="#" className="font-[600] text-[18px]">
+                Sell
+              </a>
+              <a href="#" className="font-[600] text-[18px]">
+                PG
+              </a>
+            </div>
+            <button className="  md:px-8 md:py-2 px-4 py-2 bg-blue-600 text-white rounded-[5px] text-[14px] font-[600]">
               Post Property
             </button>
             <button className="border border-blue-700 px-5 py-1 rounded-[5px]">
